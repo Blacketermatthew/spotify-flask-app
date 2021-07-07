255f1cb04339a71213f39c63d222583dbd91c016
Matthew Blacketer <matthew.blacketer@epicor.com>
2021-07-07 18:53:14 -0500

    Trying out a scheduler file (before trying scheduler heroku addon)

--- application/spotify_requests.py
+++ application/spotify_requests.py
@@ -147,8 +147,7 @@ class playlist:
     def weekly_scheduler(self):
         current_time = datetime.now().strftime("%A %H:%M")  # Returns current Day of the Week and Hour:Minute  
 
-        # if current_time == "Monday 12:00":
-        if current_time == "Wednesday 23:12":
+        if current_time == "Monday 12:00":
             print("playlist time!")
             self.insert_tracks()
         else:
