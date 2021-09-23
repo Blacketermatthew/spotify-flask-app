761281e54293666b14a2efc436c608a042e0fa63
Matthew Blacketer <matthew.blacketer@epicor.com>
2021-09-22 20:30:11 -0500

    adding heroku secret key

--- application/__init__.py
+++ application/__init__.py
@@ -3,12 +3,14 @@ from dotenv import load_dotenv
 from os import getenv, environ
 from sqlalchemy import create_engine
 
+
+
 app = Flask(__name__)
 
 # Used to configure the app when it's deployed to Heroku
 load_dotenv()
 if environ.get("IS_HEROKU"):
-    # SECRET_KEY = environ.get('SECRET_KEY')
+    SECRET_KEY = environ.get('SECRET_KEY')
     DATABASE_URI = environ.get('DATABASE_URI')
 elif getenv("IS_DEV"):
     SECRET_KEY = getenv('SECRET_KEY')
