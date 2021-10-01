adc8ac218f5cdd4a8bc261d77293709a2e7dc835
Matthew Blacketer <matthew.blacketer@epicor.com>
2021-09-30 20:54:18 -0500

    scheduler test

--- scheduler.py
+++ scheduler.py
@@ -4,7 +4,7 @@ from apscheduler.schedulers.background import BackgroundScheduler
 sched = BackgroundScheduler()
 
 # @sched.scheduled_job('cron', day_of_week='mon', hour=12, minute=0)
-@sched.scheduled_job('cron', day_of_week='thu', hour=20, minute=55)
+@sched.scheduled_job('cron', day_of_week='thu', hour=20, minute=57)
 def weekly_cronjob():
     print("Cron is now attempting to insert tracks into the database.")
     # SpotifyTracks.insert_new_tracks()
